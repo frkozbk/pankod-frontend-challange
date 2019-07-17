43cb21fac042c41e031edff04be425549f498a43
Faruk@12 <ozbekfaruk12@gmail.com>
2019-07-17 12:19:59 +0300

    Series component created

---
+++ src/components/Series/Series.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./Series.css";
+import Title from "../Title/Title";
+import Card from "../util/Card";
+
+class Series extends Component {
+    render() {
+      return (
+        <>
+          <Title title="Series" />
+          <section className="series">
+            <div className="series-container">
+              <Card
+                title="programtitle"
+                backgroundURL="https://streamcoimg-a.akamaihd.net/000/128/61/12861-PosterArt-ec32a81986a45eac7e080112075ab466.jpg"
+              />
+              <Card
+                title="programtitle"
+                backgroundURL="https://streamcoimg-a.akamaihd.net/000/128/61/12861-PosterArt-ec32a81986a45eac7e080112075ab466.jpg"
+              />
+            </div>
+          </section>
+        </>
+      );
+    }
+  }
+export default Series
