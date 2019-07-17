4db3d477c5e39024efb604fe921f3d9b9695a100
Faruk@12 <ozbekfaruk12@gmail.com>
2019-07-17 12:03:21 +0300

    Footer component added  to router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar/Navbar";
+import Footer from "./layout/Footer/Footer"
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Footer/>
     </Router>
   );
 }
