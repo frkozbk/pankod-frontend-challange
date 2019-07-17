739bd08e4be89848f12f432cb8ccdc9b5499e7a4
Faruk@12 <ozbekfaruk12@gmail.com>
2019-07-17 12:14:31 +0300

    categories route added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar/Navbar";
 import Footer from "./layout/Footer/Footer"
-
+import Categories from "./components/Categories/Categories"
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Route exact path="/" component={Categories} />
       <Footer/>
     </Router>
   );
